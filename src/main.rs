56209c7560eb4f289bc63a15c4b3375425a407aa
HarugumoFM <yuba@1-10.com>
2025-03-07 14:45:14 +0900

    feat: Implement logic to calculate maximum pattern count in string

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,29 @@ use std::cmp::min;
 use std::io::{self, BufRead};
 use std::vec;
 use std::cmp::max;
-mod adt;
-use adt::m20250305;
-
 
 fn main() {
-  m20250305::f();
+  input!{
+    N: usize,
+    S: String,
+  }
+  let S = S.chars().collect::<Vec<char>>();
+  let mut res = 1;
+  for i in 0..N {
+    //println!("{} {} {} {}", i, count1, count2, mode);
+    if S[i] == '/' {
+      let mut count = 0;
+      let mut sub = 1;
+      while(i>=sub && i+sub <N) {
+        if(S[i-sub] == '1' && S[i+sub] == '2') {
+          count += 1;
+        } else {
+          break;
+        }
+        sub+=1;
+      }
+      res = max(res, count*2+1);
+    }
+  }
+  println!("{}", res);
 }
-
